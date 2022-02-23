ea8394f258d5eeb0d31eda04b1ca11078bb5cd0b
Vector412 <100307919+VictorUnityDev@users.noreply.github.com>
2022-02-23 22:20:26 +0300

    Start

---
+++ MicroCreditApp/Assets/LoadUrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadUrl : MonoBehaviour
+{
+   public void Load()
+   {
+      Application.OpenURL("http://unity3d.com/");
+   }
+}
